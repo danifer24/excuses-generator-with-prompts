0099ce34da854b9ea071acc402888eae38c99247
danifer24 <34284277+danifer24@users.noreply.github.com>
2026-04-06 15:47:21 +0000

    generate an excuse with the random elements

--- excuse-generator.js
+++ excuse-generator.js
@@ -35,3 +35,5 @@ const randomWho = who[Math.floor(Math.random() * who.length)];
 const randomAction = action[Math.floor(Math.random() * action.length)];
 const randomWhat = what[Math.floor(Math.random() * what.length)];
 const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
