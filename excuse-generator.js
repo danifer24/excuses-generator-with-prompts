540e6de7a72b0a0fae5603c2d11e1b7628d61b49
danifer24 <34284277+danifer24@users.noreply.github.com>
2026-04-06 15:45:25 +0000

    create arrays with different parts of the excuse

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,1 +1,34 @@
 // Excuse Generator
+
+const who = [
+  'My dog',
+  'My cat',
+  'My roommate',
+  'My neighbor'
+];
+
+const action = [
+  'ate',
+  'destroyed',
+  'lost',
+  'stole',
+  'broke'
+];
+
+const what = [
+  'my homework',
+  'my laptop',
+  'my phone',
+  'my keys',
+  'my wallet'
+];
+
+const when = [
+  'this morning',
+  'yesterday',
+  'last night',
+  'today',
+  'last week'
+];
+
+
